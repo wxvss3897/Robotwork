b10c1a857f0ba3e60cd27fbbcce6d2dd0390bfec
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-03-02 14:35:14 +0200

    webstream setup

---
+++ video.py
@@ -0,0 +1,51 @@
+# MODUULI VIDEOKUVAN KÄSITTELYYN
+# ==============================
+
+# KIRJASTOT JA MODUULIT
+# ---------------------
+
+# Ulkoinen kirjasto opencv-python ladataan nimellä CV2
+import cv2
+
+# FUNKTIO, JOKA KÄYNNISTÄÄ WEB-KAMERAN JA NÄYTTÄÄ KUVAA IKKUNASSA
+# ---------------------------------------------------------------
+
+def webstream(camIx):
+    """Opens a video stream and shows frames in a window
+
+    Args:
+        camIx (int): Index of the camera starting from 0
+    """
+    # Määritellään kameraikkunan nimi
+    windowName = "Kamera " + str(camIx)
+
+    # Luodaan videostriimi
+    capture = cv2.VideoCapture(camIx)
+    
+    # IKUINEN SILMUKKA KAMERALLE
+    # ============================
+
+    # Näytetään videokuvaa niin kauan kuin sitä tulee 
+    while capture.isOpened():
+        ret, frame = capture.read()
+        
+        # Kun striimi loppuu, poistutaan silmukasta
+        if not ret:
+            print("Can't receive frames. Exiting ...")
+            break
+        
+        # Määritellään poistumisnäppäin q 
+        cv2.imshow(windowName, frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    
+    # Vapautetaan lopuksi muisti ja tuhotaan ikkuna
+    capture.release()
+    cv2.destroyAllWindows()
+
+# TESTIT
+# ======
+if __name__ == "__main__":
+    webstream(0)
+
+
